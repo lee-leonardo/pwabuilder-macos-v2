b0a72df7ebd205945384431c991a836d4381842a
lee-leonardo <lee.leonardo.king@gmail.com>
2020-11-12 12:34:15 -0800

    compartmentalize the copy and edit section

---
+++ src/copy.ts
@@ -0,0 +1,34 @@
+import * as JSZip from "jszip";
+
+// Copies files and new manifest into the zip.
+export function copyFiles(
+  zip: JSZip,
+  manifest: WebAppManifest,
+  filesAndEdits: FilesAndEdit
+) {
+  const operations = [];
+
+  for (const key of Object.keys(filesAndEdits)) {
+    operations.push(filesAndEdits[key](zip, manifest, key));
+  }
+
+  return Promise.all(operations).then((results) =>
+    results.filter((result) => !result.success)
+  );
+}
+
+export interface OperationResult {
+  filePath: string;
+  success: boolean;
+  error?: Error;
+}
+
+export type CopyAndEditFunction = (
+  zip: JSZip,
+  manifest: WebAppManifest,
+  filePath: string
+) => Promise<OperationResult>;
+
+export type FilesAndEdit = {
+  [filePath: string]: CopyAndEditFunction;
+};
