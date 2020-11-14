a202d3dde10f4da1ec749ab7a0f3594b53025153
lee-leonardo <lee.leonardo.king@gmail.com>
2020-11-13 18:26:55 -0800

    unedited folders and files left

--- src/macos.ts
+++ src/macos.ts
@@ -1,7 +1,8 @@
 import { FastifyInstance } from "fastify";
 import JSZip from "jszip";
+import { tempAppName } from "./constants";
 import { handleImages } from "./images";
-import { FilesAndEdit, copyFiles } from "./copy";
+import { FilesAndEdit, copyFiles, copyFile, copyAndEditFile } from "./copy";
 
 function schema(options: any) {
   return {
@@ -56,29 +57,122 @@ export default function macos(server: FastifyInstance, options?: any) {
 
 // Object that holds the files and edit functions to those files.
 const filesAndEdits: FilesAndEdit = {
-  "": async (zip, manifest, filePath) => {
+  "MacOSpwa/AppDelegate.swift": copyFile,
+  "MacOSpwa/Extension.swift": copyFile,
+  "MacOSpwa/Info.plist": copyFile,
+  "MacOSpwa/MacOSpwa.entitlements": copyFile,
+  "MacOSpwa/Manifest.swift": copyFile,
+  "MacOSpwa/ViewController.swift": copyFile,
+  "MacOSpwa.xcodeproj/project.pbxproj": copyAndEditFile(
+    async (content, manifest) => {
+      const name = `name = `;
+      const productName = `productName = `;
+      const testName = `name = `;
+      const uiTestName = `name = `;
+      const product_name = `PRODUCT_NAME = `;
+      const test_host = `(BUILT_PRODUCTS_DIR)/`;
+
+      return (
+        content
+          .replace(`${name}${tempAppName}`, `${name}${manifest.short_name}`)
+          .replace(
+            `${productName}${tempAppName}`,
+            `${name}${manifest.short_name}`
+          )
+          .replace(
+            `${name}${tempAppName}`,
+            `${productName}${manifest.short_name}`
+          )
+          .replace(
+            `${testName}${tempAppName + "Tests"}`,
+            `${testName}${manifest.short_name + "Tests"}`
+          )
+          .replace(
+            `${uiTestName}${tempAppName}`,
+            `${uiTestName}${manifest.short_name}`
+          )
+          // two of these
+          .replace(
+            `${product_name}${tempAppName}`,
+            `${product_name}${manifest.short_name}`
+          )
+          .replace(
+            `${product_name}${tempAppName}`,
+            `${product_name}${manifest.short_name}`
+          )
+          // two of these
+          .replace(
+            `${test_host}${tempAppName}`,
+            `${test_host}${manifest.short_name}`
+          )
+          .replace(
+            `${test_host}${tempAppName}`,
+            `${test_host}${manifest.short_name}`
+          )
+      );
+    }
+  ),
+
+  // TODO folder
+  "project/": async (zip, manifest, filePath) => {
     return {
       filePath,
       success: true,
     };
   },
-  "MacOSpwa/Base.lproj/Main.storyboard": async (zip, manifest, filePath) => {
-    try {
-      // grab the file and then make edits...
 
-      zip.file(filePath, "");
-      return {
-        filePath,
-        success: true,
-      };
-    } catch (error) {
-      return {
-        filePath,
-        success: false,
-        error,
-      };
-    }
+  // TODO folder
+  "MacOSpwa/MacOSpwa.xcdatamodeld/": async (zip, manifest, filePath) => {
+    return {
+      filePath,
+      success: true,
+    };
   },
+  // TODO folder
+  "MacOSpwa.xcodeproj/": async (zip, manifest, filePath) => {
+    return {
+      filePath,
+      success: true,
+    };
+  },
+
+  "MacOSpwa/Assets.xcassets/Contents.json": copyFile,
+  "MacOSpwa/Base.lproj/Main.storyboard": copyAndEditFile(
+    async (content, manifest) => {
+      const titleBase = `menuItem title="`;
+      const subMenuTitle = `key="submenu" title="`;
+      const aboutTitle = `title="About `;
+      const hideTitle = `title="Hide `;
+      const quitTitle = `title="Quit `;
+      const menuTitle = `menuItem title="`;
+
+      return content
+        .replace(
+          `${titleBase}${tempAppName}`,
+          `${titleBase}${manifest.short_name}`
+        )
+        .replace(
+          `${subMenuTitle}${tempAppName}`,
+          `${subMenuTitle}${manifest.short_name}`
+        )
+        .replace(
+          `${aboutTitle}${tempAppName}`,
+          `${aboutTitle}${manifest.short_name}`
+        )
+        .replace(
+          `${hideTitle}${tempAppName}`,
+          `${hideTitle}${manifest.short_name}`
+        )
+        .replace(
+          `${quitTitle}${tempAppName}`,
+          `${quitTitle}${manifest.short_name}`
+        )
+        .replace(
+          `${menuTitle}${tempAppName + "Help"}`,
+          `${menuTitle}${manifest.short_name + "Help"}`
+        );
+    }
+  ),
   "MacOSpwa/manifest.json": async (zip, manifest, filePath) => {
     try {
       zip.file(filePath, JSON.stringify(manifest, undefined, 2));
