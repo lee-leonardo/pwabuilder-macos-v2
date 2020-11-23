b977488eec8cf64bfb9a4b1fd7cb6b6cd3ff730d
lee-leonardo <lee.leonardo.king@gmail.com>
2020-11-23 11:53:50 -0800

    remove noise atm.

--- build/macos.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="node" />
-import { FastifyInstance } from "fastify";
-export default function macos(fastify: FastifyInstance, options?: any): FastifyInstance<import("http").Server, import("http").IncomingMessage, import("http").ServerResponse, import("fastify").FastifyLoggerInstance>;
